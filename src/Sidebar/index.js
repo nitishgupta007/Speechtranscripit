0b8bb11d7f74e91c5999aca7b31f7a650baa0bd5
nitishgupta7 <nitish@sendinblue.com>
2019-06-09 17:19:10 +0530

    correction

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Sidebar extends Component {
     render() {
         return (
             <ul className="sidebar navbar-nav">
                 <li className="nav-item active">
-                <a className="nav-link" href="index.html">
-                    <svg width="16" height="16" viewBox="0 0 512 512">
-                                        <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm57.1 350.1L224.9 294c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h48c6.6 0 12 5.4 12 12v137.7l63.5 46.2c5.4 3.9 6.5 11.4 2.6 16.8l-28.2 38.8c-3.9 5.3-11.4 6.5-16.8 2.6z"></path></svg>
-                                Recent jobs<span className="sr-only">(current)</span>
-                </a>
+                    <Link className="nav-link" to="/listing">
+                        <svg width="16" height="16" viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm57.1 350.1L224.9 294c-3.1-2.3-4.9-5.9-4.9-9.7V116c0-6.6 5.4-12 12-12h48c6.6 0 12 5.4 12 12v137.7l63.5 46.2c5.4 3.9 6.5 11.4 2.6 16.8l-28.2 38.8c-3.9 5.3-11.4 6.5-16.8 2.6z"></path>
+                        </svg> Recent jobs
+                    </Link>
                 </li>
                 
                 <li className="nav-item">
-                    <a className="nav-link" href="charts.html">
-                    <svg width="16" height="16" viewBox="0 0 512 512">
-                        <path fill="currentColor" d="M512 176.001C512 273.203 433.202 352 336 352c-11.22 0-22.19-1.062-32.827-3.069l-24.012 27.014A23.999 23.999 0 0 1 261.223 384H224v40c0 13.255-10.745 24-24 24h-40v40c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24v-78.059c0-6.365 2.529-12.47 7.029-16.971l161.802-161.802C163.108 213.814 160 195.271 160 176 160 78.798 238.797.001 335.999 0 433.488-.001 512 78.511 512 176.001zM336 128c0 26.51 21.49 48 48 48s48-21.49 48-48-21.49-48-48-48-48 21.49-48 48z" >
-                        </path>
-                    </svg> Access token
-                    </a>
+                    <Link className="nav-link" to="/">
+                        <svg width="16" height="16" viewBox="0 0 512 512">
+                            <path fill="currentColor" d="M512 176.001C512 273.203 433.202 352 336 352c-11.22 0-22.19-1.062-32.827-3.069l-24.012 27.014A23.999 23.999 0 0 1 261.223 384H224v40c0 13.255-10.745 24-24 24h-40v40c0 13.255-10.745 24-24 24H24c-13.255 0-24-10.745-24-24v-78.059c0-6.365 2.529-12.47 7.029-16.971l161.802-161.802C163.108 213.814 160 195.271 160 176 160 78.798 238.797.001 335.999 0 433.488-.001 512 78.511 512 176.001zM336 128c0 26.51 21.49 48 48 48s48-21.49 48-48-21.49-48-48-48-48 21.49-48 48z" >
+                            </path>
+                        </svg> Access token
+                    </Link>
                 </li>
             </ul>
         );
